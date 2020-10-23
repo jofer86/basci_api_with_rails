7178fbec29ff36dbf91594d4bfd96b766f91f846
Jorge Rincon <jofer86@gmail.com>
2020-10-22 20:21:30 -0500

    creates method for the orders controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 class Product < ApplicationRecord
   belongs_to :user
+  has_many :placements, dependent: :destroy
+  has_many :orders, through: :placements
+
+
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :title, :user_id, presence: true
   
